052d6e3dfd64dba5c58b201adb18bdf6d3606a23
kkalla <dkswns333@gmail.com>
2017-07-18 09:10:49 +0900

    Update how to use twitter api in R

--- usingTwitterAPI.R
+++ usingTwitterAPI.R
@@ -1,3 +1,26 @@
 ## Using twitterAPI
-install.packages("twitteR")
+## install.packages("twitteR")
 library(twitteR)
+library(httr)
+api <- read.csv("twitter_api.txt",header = FALSE)
+api_key <- as.character(api[1,1])
+api_secret <- as.character(api[1,2])
+access_token <- as.character(api[2,1])
+access_token_secret <- as.character(api[2,2])
+
+setup_twitter_oauth(api_key,api_secret,
+                    access_token = access_token,
+                    access_secret = access_token_secret)
+
+## search tweets
+tweets <- searchTwitter("#수원",n=10,lang = "ko")
+head(tweets)
+head(strip_retweets(tweets))
+
+## looking at users
+user <- getUser('ebay')
+user$getFriends(n=5)
+
+## Conversion to data.frames
+df <- twListToDF(tweets)
+df
